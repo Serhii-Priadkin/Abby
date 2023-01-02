6c0f6a300c4865dc65c8953ece4062e6a906e7fa
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-01-02 20:36:38 +0200

    CRUD complite

--- AbbyWeb/Pages/Categories/Edit.cshtml.cs
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -7,17 +7,18 @@ using System.Threading.Tasks;
 namespace AbbyWeb.Pages.Categories
 {
     [BindProperties]
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
      
         public Category Category { get; set; }
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public void OnGet()
+        public void OnGet(int id)
         {
+            Category = _db.Category.Find(id);
         }
 
         public async Task<IActionResult> OnPost(Category category)
@@ -28,7 +29,7 @@ namespace AbbyWeb.Pages.Categories
             }
             if (ModelState.IsValid)
             {
-                await _db.Category.AddAsync(Category);
+                _db.Category.Update(Category);
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
             }
